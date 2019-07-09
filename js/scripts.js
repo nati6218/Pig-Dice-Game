8553ffcd298d967508583c15f17545be1da734c3
Student <example@example.com>
2019-07-08 20:19:50 -0700

    add function and prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,41 @@
+function Game(player1, player2, score){
+  this.player1 = player1;
+  this.player2 = player2;
+  this.score = score;
+
+}
+
+Game.prototype.roll = function(){
+
+    var dice = Math.floor(Math.random());
+    var score = [];
+
+    for(var = dice; dice =>2; dice++){
+
+
+    if (dice === 1) {
+      return "Game Over"
+    } else{
+      this.score.push(dice);
+
+    }
+  }
+
+Game.prototype.stop = function(){
+  this.score = 1;
+}
+Game.prototype.win = function(){
+  if(this.score >= 100){
+    return "Victory! Congratulations"
+  }
+}
+
+
 $(document).ready(function(){
-  $('#game').cl
+  $('#game').click(function(event){
+    event.preventDefault();
+    
+
+  });
 
 });
