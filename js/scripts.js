4ee251ea6d98b60462e12b2f2b2da7237482a268
Natalia Tigh <natih6218@gmail.com>
2019-07-10 20:19:26 -0700

    add different functionality to players

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,19 @@
-function Game(player1, player2, score){
-  this.player1 = player1;
-  this.player2 = player2;
+function Game(player1score, player2score, passTurn, score){
+  this.player1score = player1score;
+  this.player2score = player2score;
+  this.passTurn = passTurn;
   this.score = 0;
 
 }
 
-Game.prototype.roll = function(){
+Game.prototype.roll = function() {
 
   var dice = Math.floor(Math.random() * 6) + 1;
-  var score = dice + dice;
-  console.log(dice);
+  this.player1score += dice;
+  this.player2score += dice;
 
   if (dice === 1){
     alert ("You lost! Sorry");
-  }else {
-    alert(score);
   }
 }
 
@@ -24,39 +23,45 @@ Game.prototype.roll = function(){
 //   this.score.push(dice);
 //
 // }
-
-Game.prototype.stop = function(){
-  if (this.score === 1){
-    return "Sorry! you lost"
-  };
-
+Game.prototype.stop= function(){
+  this.score += this.passTurn
+  this.passTurn = 0;
 }
 
 Game.prototype.win = function(){
-  if(this.score >= 100){
+  if(this.player1score >= 100){
     return "Victory! Congratulations"
   }
 }
 
 
 $(document).ready(function(){
-  var newGame = new Game('player1', 'player2', 0);
+  var newGame = new Game(0, 0, 0, 0);
+
+
   $('#game').submit(function(event){
     event.preventDefault();
 
-    var player1 = $('#player1')
-
-    $('#points').show();
-
 
   });
 
-  $('#play1, #play2').click(function(){
+  $('#play1').click(function(){
     newGame.roll();
+    newGame.win();
+    $('.points1').text(newGame.player1score);
+
+  });
+  
+  $('#play2').click(function(){
+    newGame.roll();
+    newGame.win();
+    $('.points2').text(newGame.player2score);
+
   });
 
-  $('#stop').click(function(){
-    newGame.stop();
+
+  $('.stop').click(function(){
+    newGame.stop(alert("Pass"));
   });
 
 });
